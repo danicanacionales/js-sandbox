c1a866920220caeebaf1b89870f14a10431151f4
danica nacionales <danicanacionales@gmail.com>
2019-05-08 22:23:39 +0800

    githubfinder
    may 8, 2019

--- section 8/app.js
+++ section 8/app.js
@@ -10,12 +10,13 @@ UIsearchUser.addEventListener('keyup', (e) => {
         github.getUser(userText)
             .then(data => {
                 if(data.profile.message === 'Not Found'){
-
+                    ui.showAlert('User not found', 'alert alert-danger');
                 }else{
                     ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
                 }
             })
     }else{
-
+        ui.clearProfile();
     }
 });
--- section 8/github.js
+++ section 8/github.js
@@ -1,15 +1,21 @@
 class GitHub {
     constructor() {
         this.client_id = 'a22ca87673ddaae098fc'
-        this.client_secret = 'ba224af1c314190766263bb6087b2455cadde573'
+        this.client_secret = 'ba224af1c314190766263bb6087b2455cadde573';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
